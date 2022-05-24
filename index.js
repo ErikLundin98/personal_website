3172e5b286056075a4d14add3839ddabbeb8a8ea
Erik Lundin <erilu186@student.liu.se>
2022-05-24 19:10:30 +0200

    unnecessary changes

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+ * Terminal stuff
+ */
+
 howTo = "\
 Big thanks to Jakub T. Jankiewicz for creating this tool and providing a helpful guide on how to create this website: https://itnext.io/how-to-create-interactive-terminal-like-website-888bb0972288 \
 ";
@@ -8,7 +12,7 @@ const greeting = "[[gb;yellow;black]\n\n\
 : `;  .--. .-.: `'.'  : :   .-..-.,-.,-. .-' :.-.,-.,-.\n\
 : :__ : ..': :: . `.  : :__ : :; :: ,. :' .; :: :: ,. :\n\
 `.__.':_;  :_;:_;:_;  :___.'`.__.':_;:_;`.__.':_;:_;:_;\n\
-\n\
+\n\n\n\n\n\n\
 Welcome to my personal website! For a list of available commands, type \"help\" and press enter\n\
 -----------------------------------------------------------------------------------------------]";
 
@@ -39,6 +43,8 @@ socials  :  links to my social pages\n\
 how      :  how was this site created?\n\
 projects :  a sorted list of my GitHub projects\n\
 resume   :  download my resume\n\
+hello    :  see initial display\n\
+clear    :  clear display\n\
 ";
 
 $.terminal.defaults.allowedAttributes.push('style');
@@ -67,6 +73,9 @@ function get_github() {
 }
 
 $('body').terminal({
+    hello: function() {
+      this.echo(greeting);
+    },
     how: function() {
         this.echo(howTo);
     },
@@ -92,6 +101,16 @@ $('body').terminal({
         link.target="_blank";
         link.download = "url";
         link.dispatchEvent(new MouseEvent('click'));
+    },
+    python: function() {
+      return $('<iframe src="https://trinket.io/embed/python/3d8d7ce66b?runOption=console" width="100%" height="356" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>');
+    },
+    color: function(color) {
+      this.echo("changing color to " + color, {
+        finalize: function(div) {
+            div.css("color", color);
+        }
+    });
     }
 
 }, {
